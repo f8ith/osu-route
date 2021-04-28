4669273463967f9c30575456da1f750bbc124976
faith <jamienibtong@gmail.com>
2021-04-28 16:41:42 +0800

    changed downloader to use set id, osu!route functions correctly

--- osuroute/cli.py
+++ osuroute/cli.py
@@ -11,23 +11,37 @@ import urllib.parse
 import psutil
 
 if platform.system() == "Linux":
-    osu_path = "/mnt/HDD/osu"
-    firefox_path = "/usr/bin/firefox"
+    osu_path = Path("/mnt/HDD/osu")
+    firefox_path = Path("/usr/bin/firefox")
+    osu = "osu"
 else:
-    osu_path = "c:/Users/jamie/"
+    osu_path = Path("d:/osu/")
     firefox_path = "c:/Program Files/Mozilla Firefox/firefox.exe"
+    osu = osu_path / "osu!.exe"
 
 
 def main():
     parser = argparse.ArgumentParser("osu-route")
     parser.add_argument("link", type=str)
     args = parser.parse_args()
-    if args.link.startswith("https://osu.ppy.sh/b/"):
+    processes = psutil.process_iter()
+    ret = "osu!.exe" in (p.name() for p in processes)
+    if ret:
+        if args.link.startswith("https://osu.ppy.sh/b/") or args.link.startswith(
+            "https://osu.ppy.sh/beatmaps/"
+        ):
+            beatmap_id = args.link.split("/")[4]
+            beatmap = requests.get(f"https://api.chimu.moe/v1/map/{beatmap_id}")
+            set_id = beatmap.json()["data"]["ParentSetId"]
+        elif args.link.startswith("https://osu.ppy.sh/beatmapsets/"):
+            set_id = args.link.split("/")[4]
+        else:
+            subprocess.run([Path(firefox_path), "-osint", "-url", args.link])
+            return
         processes = psutil.process_iter()
         ret = "osu!.exe" in (p.name() for p in processes)
         if ret:
-            beatmap = args.link.split("/")[4]
-            r = requests.get(f"https://chimu.moe/d/{beatmap}", stream=True)
+            r = requests.get(f"https://chimu.moe/d/{set_id}", stream=True)
             d = r.headers["Content-Disposition"]
             filename = (
                 urllib.parse.unquote(d.split('filename="')[1].split('";')[0])
@@ -35,9 +49,11 @@ def main():
                 .replace('"', "")
                 .replace("*", " ")
             )
-            with open(Path(osu_path) / filename, "wb") as f:
+            filepath = osu_path / filename
+            with open(filepath, "wb") as f:
                 for chunk in r.iter_content(4096):
                     f.write(chunk)
+            subprocess.run([osu, filepath])
             return
     subprocess.run([Path(firefox_path), "-osint", "-url", args.link])
 
